ec45483f6562e5520b834541f9b82ea8c1e73b86
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-10-01 22:01:29 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new 
     req = Rack::Request.new(env)
     
-    if req.path.match == "/items/"
+    if req.path.match(/items/)
       
     else 
       resp.write "Item not found"
