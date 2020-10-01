022501c758398ca91bea14fc6e3a8fa1213ecc74
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-10-01 22:03:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,6 @@ class Application
     if req.path.match(/items/)
       search_term = req.params["item"]
       if @@items.include?(search_term)
-        @@cart << search_term
         resp.write "added #{search_term}"
       else 
         resp.write "We don't have that item"
