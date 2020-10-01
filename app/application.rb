a466dcf3d89f696704f6a4250851a3c180ee97a3
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-10-01 22:02:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,14 @@ class Application
     req = Rack::Request.new(env)
     
     if req.path.match(/items/)
-      
+      search_term = req.params["item"]
+      if @@items.include?(search_term)
+        @@cart << search_term
+        resp.write "added #{search_term}"
+      else 
+        resp.write "We don't have that item"
+      end 
+
     else 
       resp.write "Item not found"
       resp.status = 400 
