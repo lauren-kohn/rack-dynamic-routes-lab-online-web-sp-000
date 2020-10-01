506bd78d8c09096d0de50227bd748b84855ed3bf
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-10-01 21:59:28 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+class Application 
+  
+  def call(env)
+    resp = Rack::Response.new 
+    req = Rack::Request.new(env)
+    
+    if req.path == "/items/"
+      
+    else 
+      resp.write "Item not found"
+      resp.status = 400 
+    end 
+    
+    resp.finish 
+  end
+  
+end 
