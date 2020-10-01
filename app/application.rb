d7dc4de8370ed6fb63fdea0cd42058b0c68f027e
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-10-01 22:33:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if item_name = @@items.find{|i| i.name}
+      if item_name = @@items.find{ |i| i.name == item_name}
         resp.write item.price
         resp.status = 200
       else 
