96132aef66d58ca97e2124c8ad04347faf02a371
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-10-01 22:32:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     req = Rack::Request.new(env)
     
     if req.path.match(/items/)
-      item_name = req.path.split("items")
+      item_name = req.path.split("/items/").last
       if @@items.include?(search_term)
         resp.write "#{search_term.price}"
         resp.status = 200
